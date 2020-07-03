bbeb87d9bc37d99a70ec14860cf1bae1266de8d8
Phernandoe <ferbar811@gmail.com>
2020-07-02 21:03:23 -0500

    lets get it

--- bujo/bujo-cli.py
+++ bujo/bujo-cli.py
@@ -3,7 +3,18 @@ import click
 
 @click.command()
 def main():
-    click.echo("this is bujito y bujitas CLI")
+    click.echo("""
+    Welcome to the
+    
+        .-.                    .----.        
+  (_) )-.    _     .-.      /.--.    .- 
+     / __)  '     (        //    )`-'   
+    /    `.  /     )..-.   //    /       
+   /'      )(     / `.   /(    /        
+(_/  `----'  `._.'    `-'  `-.'         
+
+    CLI
+    """)
 
 if __name__ == "__main__":
     main()
